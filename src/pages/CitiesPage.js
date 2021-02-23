c18660853a3aaffd807d4d21d1f7932664487611
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-02-22 20:07:00 -0600

    removes curly brace

--- src/pages/CitiesPage.js
+++ src/pages/CitiesPage.js
@@ -1,12 +1,12 @@
 import CitiesList from '../components/CitiesList';
 
 const CitiesPage = () => {
-  return (
-    <aside>
-      <header>Cities</header>
-      {CitiesList}
-    </aside>
-  );
+	return (
+		<aside>
+			<header>Cities</header>
+			<CitiesList />
+		</aside>
+	);
 };
 
 export default CitiesPage;
