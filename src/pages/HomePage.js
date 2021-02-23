60a8178f606aabed9c8917870e3a678aa82d9564
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-02-23 01:38:27 -0600

    adds navbar styling and hompage styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,31 +1,52 @@
+import Carousel from '../components/universalComp/Carousel';
+
 const HomePage = () => {
-  return (
-    <div>
-      <div>
-        {/* Image Carousel */}
-        <h1>Image Carousel Goes Here...</h1>
-      </div>
+	return (
+		<div id="home-container">
+			<div id="carousel-container">
+				<Carousel />
+			</div>
 
-      <div>
-        <h1>Wayfarer is ...</h1>
-      </div>
-
-      <div>
-        <article>
-          <h2>Topic</h2>
-          <p>Nulla ea ullamco ad id anim et. Mollit ullamco est commodo aute reprehenderit elit voluptate. Lorem est amet nulla consequat cillum elit in cillum sunt occaecat aliqua consequat. Voluptate fugiat laborum nostrud quis ex anim enim est dolor enim nulla. Ullamco culpa occaecat tempor mollit ut ex et eiusmod.</p>
-        </article>
-        <article>
-          <h2>Topic</h2>
-          <p>Nulla ea ullamco ad id anim et. Mollit ullamco est commodo aute reprehenderit elit voluptate. Lorem est amet nulla consequat cillum elit in cillum sunt occaecat aliqua consequat. Voluptate fugiat laborum nostrud quis ex anim enim est dolor enim nulla. Ullamco culpa occaecat tempor mollit ut ex et eiusmod.</p>
-        </article>
-        <article>
-          <h2>Topic</h2>
-          <p>Nulla ea ullamco ad id anim et. Mollit ullamco est commodo aute reprehenderit elit voluptate. Lorem est amet nulla consequat cillum elit in cillum sunt occaecat aliqua consequat. Voluptate fugiat laborum nostrud quis ex anim enim est dolor enim nulla. Ullamco culpa occaecat tempor mollit ut ex et eiusmod.</p>
-        </article>
-      </div>
-    </div>
-  );
+			<div className="m-6">
+				<h1 className="text-4xl">
+					Wayfarer is an application, where we provide cities of the world and
+					the environment to contribute comments and reviews of these locations.
+				</h1>
+			</div>
+			<div id="home-item-container">
+				<article className="home-item">
+					<h2 className="text-4xl">Topic 1</h2>
+					<p>
+						Nulla ea ullamco ad id anim et. Mollit ullamco est commodo aute
+						reprehenderit elit voluptate. Lorem est amet nulla consequat cillum
+						elit in cillum sunt occaecat aliqua consequat. Voluptate fugiat
+						laborum nostrud quis ex anim enim est dolor enim nulla. Ullamco
+						culpa occaecat tempor mollit ut ex et eiusmod.
+					</p>
+				</article>
+				<article className="home-item">
+					<h2 className="text-4xl">Topic 2</h2>
+					<p>
+						Nulla ea ullamco ad id anim et. Mollit ullamco est commodo aute
+						reprehenderit elit voluptate. Lorem est amet nulla consequat cillum
+						elit in cillum sunt occaecat aliqua consequat. Voluptate fugiat
+						laborum nostrud quis ex anim enim est dolor enim nulla. Ullamco
+						culpa occaecat tempor mollit ut ex et eiusmod.
+					</p>
+				</article>
+				<article className="home-item">
+					<h2 className="text-4xl">Topic 3</h2>
+					<p>
+						Nulla ea ullamco ad id anim et. Mollit ullamco est commodo aute
+						reprehenderit elit voluptate. Lorem est amet nulla consequat cillum
+						elit in cillum sunt occaecat aliqua consequat. Voluptate fugiat
+						laborum nostrud quis ex anim enim est dolor enim nulla. Ullamco
+						culpa occaecat tempor mollit ut ex et eiusmod.
+					</p>
+				</article>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
