35515c4b96b5ff9d7a22d7599abbfc036a632ca6
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-02-23 13:55:57 -0600

    changed some stuff

--- src/components/CitiesList.js
+++ src/components/CitiesList.js
@@ -2,22 +2,11 @@ import React from 'react';
 import CitiesCard from './CitiesCard';
 
 const CitiesList = (props) => {
-  return props.cities.map((citiesObj) => {
-    console.log(citiesObj);
-    return (
-      <>
-        <header>
-          <h1>Cities</h1>
-        </header>
-        <ul>
-          <CitiesCard
-            key={citiesObj._id}
-            cities={citiesObj}
-          />
-        </ul>
-      </>
-    );
-  });
+	const cities = props.cities.map((citiesObj) => {
+		return <CitiesCard key={citiesObj._id} cities={citiesObj} />;
+	});
+
+	return <ul>{cities}</ul>;
 };
 
 export default CitiesList;
