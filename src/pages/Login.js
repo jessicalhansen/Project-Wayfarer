1c892c6fb58f85dba90c8f3adf18eb9763fe35e2
jessicalh <jessicalynnehansen@gmail.com>
2021-02-25 15:14:12 -0700

    Updated Sign-up page styling.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -63,38 +63,6 @@ class Login extends React.Component {
 				</div>
 			</div>
 			</div>
-
-			/* <div className="flex justify-center content-center">
-						<form onSubmit={this.loginHandler}>
-					<div>
-						<label className="form-label" htmlFor="name">
-							Username
-						</label>
-						<input
-							className="form-control"
-							type="text"
-							id="username"
-							name="username"
-							value={this.state.username}
-							onChange={this.inputChangeHandler}
-						/>
-					</div>
-					<div>
-						<label className="form-label mt-2" htmlFor="email">
-							Password
-						</label>
-						<input
-							className="form-control"
-							type="password"
-							id="password"
-							name="password"
-							value={this.state.password}
-							onChange={this.inputChangeHandler}
-						/>
-					</div>
-					<button type="submit" className="btn m-2 btn-primary hover:bg-green-600">Login</button>
-				</form>
-			</div> */
 		)
 	}
 }
