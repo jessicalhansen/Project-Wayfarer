5945de3780332083703a14a89ffa28a7f2f5e243
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-02-25 18:17:31 -0600

    adds user route in backend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,48 +22,62 @@ class Login extends React.Component {
 	render() {
 		return (
 			<div className="h-screen login bg-cover">
-			<div className="container mx-auto h-full flex flex-1 justify-center">
-				<div className="w-full max-w-lg">
-					<div className="leading-loose ml-10">
-						<form className="loginform max-w-sm m-4 p-10 rounded" onSubmit={this.loginHandler}>
-							<p className="text-white font-medium text-center text-lg font-bold mb-3">LOGIN</p>
+				<div className="container mx-auto h-full flex flex-1 justify-center">
+					<div className="w-full max-w-lg">
+						<div className="leading-loose ml-10">
+							<form
+								className="loginform max-w-sm m-4 p-10 rounded"
+								onSubmit={this.loginHandler}
+							>
+								<p className="text-white font-medium text-center text-lg font-bold mb-3">
+									LOGIN
+								</p>
 								<div className="">
 									<label htmlFor="name" className="block text-sm text-white">
-									Username
+										Username
 									</label>
-									<input 
-									className="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white"
-									type="text"
-									id="username"
-									name="username"
-									value={this.state.username}
-									onChange={this.inputChangeHandler}
+									<input
+										className="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white"
+										type="text"
+										id="username"
+										name="username"
+										value={this.state.username}
+										onChange={this.inputChangeHandler}
+										required
 									/>
 								</div>
 								<div className="mt-2">
-									<label className="block text-sm text-white" htmlFor="password">
+									<label
+										className="block text-sm text-white"
+										htmlFor="password"
+									>
 										Password
 									</label>
-									<input className="w-full px-5 py-1 text-gray-7000 bg-gray-300 rounded focus:outline-none focus:bg-white"
-									type="password"
-									id="password"
-									name="password"
-									value={this.state.password}
-									onChange={this.inputChangeHandler}
+									<input
+										className="w-full px-5 py-1 text-gray-7000 bg-gray-300 rounded focus:outline-none focus:bg-white"
+										type="password"
+										id="password"
+										name="password"
+										value={this.state.password}
+										onChange={this.inputChangeHandler}
+										required
 									/>
 								</div>
 
 								<div className="mt-4 items-center">
-									<button type="submit" className="px-4 py-1 text-white font-light tracking-wider bg-gray-400 hover:bg-gray-800 rounded">
-									Login
+									<button
+										type="submit"
+										className="px-4 py-1 text-white font-light tracking-wider bg-gray-400 hover:bg-gray-800 rounded"
+									>
+										Login
 									</button>
-								</div>	
-						</form>
+								</div>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
-			</div>
-		)
+		);
 	}
 }
 
