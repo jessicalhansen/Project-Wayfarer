3d78d9dc8a8dc8147ba62ae4a4ed988d4107eb2c
nicholasgacicia <nicholasgacicia@gmail.com>
2021-02-26 01:01:48 -0600

    Updated README

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -14,19 +14,19 @@ const HomePage = () => {
 			</div>
 			<div id="home-item-container">
 				<article className="home-item bg-yellow-50">
-					<h2 className="text-4xl mb-5">a Platform</h2>
+					<h2 className="text-4xl mb-3">a Platform</h2>
 					<p>
 						... to share your travel advice, knowledge and first-hand experiences of your travel adventures to destinations around the globe.
 					</p>
 				</article>
 			<article className="home-item bg-yellow-50">
-					<h2 className="text-4xl mb-5">a Resource</h2>
+					<h2 className="text-4xl mb-3">a Resource</h2>
 					<p>
 						... whether it's you're first time leaving the the nest or a seasoned traveling veteran, there is always a new place to discover on Wayfarer.
 					</p>
 				</article>
 			<article className="home-item bg-yellow-50">
-					<h2 className="text-4xl mb-5">a Community</h2>
+					<h2 className="text-4xl mb-3">a Community</h2>
 					<p>
 						... to connect you with like-minded individuals who have a passion for travel and advice to plan your next big adventure.
 					</p>
